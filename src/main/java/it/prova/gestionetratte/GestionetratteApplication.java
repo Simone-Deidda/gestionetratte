e7a6fcc85f43f751a520cfe126b144600dde065f
Simone-Deidda <deidda.simone94@gmail.com>
2022-06-22 15:50:35 +0200

    Commit iniziale

---
+++ src/main/java/it/prova/gestionetratte/GestionetratteApplication.java
@@ -0,0 +1,20 @@
+package it.prova.gestionetratte;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionetratteApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionetratteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/it/prova/gestionetratte/model/Airbus.java
@@ -0,0 +1,5 @@
+package it.prova.gestionetratte.model;
+
+public class Airbus {
+	//id, codice, descrizione, dataInizioServizio, numeroPasseggeri, tratte
+}
